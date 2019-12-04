52a3e146c1de42675b7f95a6212425d563da8dbe
josperdom1 <josperdom1@gmail.com>
2019-12-04 20:49:47 +0100

    feat(booth): Voting list on Angular
    Angular app has been  successfully connected to Django REST framework.
    	+ Angular model for Voting entity: id, name.
    	+ Lisintg by voting name (no css applied)

--- decide/booth/angular/src/app/app.component.ts
+++ decide/booth/angular/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Voting } from './voting.model';
+import { DataService } from './data.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements OnInit {
+  votings: Voting[];
+
+  // constructor(private api: ApiService) {
+  //   this.getVoting();
+  // }
+
+
+  // (res: any) => {
+  //   console.log(res);
+  // }
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    return this.dataService.getVotings()
+    .subscribe(data => this.votings = data);
+
+  }
+
+  // getVoting = () => {
+  //   this.api.getVotings().subscribe(
+  //     data => {
+  //       this.votings = data;
+  //     },
+  //     error => {
+  //       console.log(error);
+  //     }
+  //   );
+  // };
+
 }
---
+++ decide/booth/angular/src/app/data.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Voting } from './voting.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  baseurl = 'http://127.0.0.1:8000/voting';
+
+  constructor(private http: HttpClient) { }
+
+  getVotings() {
+    return this.http.get<Voting[]>(this.baseurl);
+  }
+}
