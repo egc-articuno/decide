24a3854b0d045ad7f098bd25cd582ec595578316
Daniel Negrón <danielnf@hotmail.es>
2020-01-07 17:59:32 +0100

    fix(postproc): Added branch for deployment
    Travis and Heroku configuration files have been modified to deploy the postproc module.
    
    <footer>

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -72,7 +72,7 @@ MODULES = [
 ]
 
 #BASEURL = 'http://localhost:8000'
-BASEURL = 'https://decide-articuno.herokuapp.com/'
+BASEURL = 'https://articuno-postproc.herokuapp.com/'
 
 APIS = {
     'authentication': BASEURL,
