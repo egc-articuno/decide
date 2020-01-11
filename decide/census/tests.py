136f20162aae49673cc63787b0860936c92ae6a8
claguecue <claguecue@alum.us.es>
2020-01-11 12:45:25 +0100

    Test(Census): Add test to create census Add test to see the create census by postal code Issue -> 5 (census)
    Tipos:
    	Feat: Nuevo característica.
    	Fix: Arreglar bug.
    	Docs: Cambiar documentación.
    	Style: Formato, colores, y cualquier cosa relaciona, que no requiere de código que no sea css.
    	Refactor: Refactorizar el código de producción.
    	Test: Añadir o actualizar pruebas.
    	Chore:  Actualizar tareas difíciles, sin cambiar el código de producción.

--- decide/census/tests.py
+++ decide/census/tests.py
@@ -90,3 +90,21 @@ class CensusTestCase(BaseTestCase):
 
         census_after = len(Census.objects.all().values_list('voting_id', flat=True))
         self.assertTrue(census_before < census_after)
+    
+
+    def test_save_census_CP(self):
+        data = {'postal_code':11223}
+        census_1 = len(Census.objects.all().values_list('voting_id', flat=True))
+
+
+        admin = User(email='administrador@gmail.com', password='qwerty')
+        admin.is_staff = True
+        admin.save()
+
+        self.client.force_login(admin)
+
+        response = self.client.get('/census/saveNewCensusCP', {'postal_code':11223})
+
+        census_2 = len(Census.objects.all().values_list('voting_id', flat=True))
+        self.assertTrue(census_1 == census_2)
+        self.assertEqual(response.status_code, 302)
