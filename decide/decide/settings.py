f1e4868c3e62df35c1191499ba60617722b964fe
Ivan <ivalopher10@gmail.com>
2020-01-12 23:32:45 +0100

    arreglo static

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -182,8 +182,7 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,"static"),]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 161
