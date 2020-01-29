8d84b91c1c51bb469c11844ad954e617ef67ba68
José Andrés Pérez Domínguez <37655841+josperdom1@users.noreply.github.com>
2020-01-29 20:03:55 +0100

    Keybits reduced

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -193,7 +193,7 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 STATIC_URL = '/static/'
 
 # number of bits for the key, all auths should use the same number of bits
-KEYBITS = 161
+KEYBITS = 16
 
 # Versioning
 ALLOWED_VERSIONS = ['v1', 'v2']
